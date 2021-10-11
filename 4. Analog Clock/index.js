8755dc2088642fdaf7f284433087b28803470590
jhyj0521 <jhyj3636@gmail.com>
2021-10-11 17:49:01 +0900

    fix: Analog Clock refatoring

--- 4. Analog Clock/index.js
+++ 4. Analog Clock/index.js
@@ -4,17 +4,13 @@ const $second = document.querySelector('.second');
 
 window.addEventListener('DOMContentLoaded', () => {
   setInterval(() => {
-    const today = new Date().setHours(0, 0, 0, 0);
-    const now = new Date();
-    const totalSecond = Math.round((now - today) / 1000);
-    const hour = (totalSecond / 3600) % 12;
-    const minute = totalSecond / 60 - now.getHours() * 60;
-    const second = Math.ceil(
-      totalSecond - now.getHours() * 3600 - now.getMinutes() * 60
-    );
+    const today = new Date();
+    const hour = today.getHours();
+    const minute = today.getMinutes();
+    const second = today.getSeconds();
 
-    $hour.style.setProperty('--deg', hour * 30);
-    $minute.style.setProperty('--deg', minute * 6);
+    $hour.style.setProperty('--deg', (hour + minute / 60 + second / 3600) * 30);
+    $minute.style.setProperty('--deg', (minute + second / 60) * 6);
     $second.style.setProperty('--deg', second * 6);
   }, 1000);
 });
