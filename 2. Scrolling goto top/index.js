cd789fed87ac946b9050deb5d0904a5c3f128229
jhyj0521 <jhyj3636@gmail.com>
2021-10-11 16:00:28 +0900

    feat: Scrolling goto top

---
+++ 2. Scrolling goto top/index.js
@@ -0,0 +1,31 @@
+const $scrollIcon = document.querySelector('.scroll-icon');
+
+const throttle = (callback, delay) => {
+  let timerId;
+
+  return event => {
+    if (timerId) return;
+    timerId = setTimeout(
+      () => {
+        callback(event);
+        timerId = null;
+      },
+      delay,
+      event
+    );
+  };
+};
+
+window.addEventListener(
+  'scroll',
+  throttle(() => {
+    $scrollIcon.style.display = window.pageYOffset >= 100 ? 'block' : 'none';
+  }, 100)
+);
+
+$scrollIcon.onclick = () => {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  });
+};
