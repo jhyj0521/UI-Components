01ef6f1295a8e1bdd9727223c42435164300e1b0
hhhyyo <gydnjsrh6923@korea.ac.kr>
2021-10-13 10:50:32 +0900

    feat: Toaster

--- 10. Toaster/index.js
+++ 10. Toaster/index.js
@@ -1,4 +1,34 @@
-const toaster = null;
+const $body = document.querySelector('body');
+
+const toaster = (() => {
+  let count = 0;
+  return {
+    add({ type, title, message }) {
+      const $div = document.createElement('div');
+      $div.innerHTML = `
+        <h4 class="toast-heading">${title} ${count}</h4>
+        <div class="toast-message">
+          <svg width="24" height="24">
+            <use xlink:href="#${type}" />
+          </svg>
+          <p>${message}</p>
+        </div>
+        <a class="close">&times;</a>
+      `;
+
+      $div.setAttribute('class', `toast toast-${type}`);
+
+      $body.appendChild($div);
+
+      count += 1;
+
+      setTimeout(() => {
+        $body.removeChild($div);
+        count -= 1;
+      }, 3000);
+    }
+  };
+})();
 
 const TOAST_TYPE = {
   SUCCESS: 'success',
