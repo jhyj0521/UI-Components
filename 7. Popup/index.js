8a2b891c775269833fefc5d8ac34aefa8e5eccd3
jhyj0521 <jhyj3636@gmail.com>
2021-10-15 21:34:49 +0900

    refactor: Popup

--- 7. Popup/index.js
+++ 7. Popup/index.js
@@ -1,31 +1,19 @@
 const $openButton = document.querySelector('.open-button');
 const $modalBackground = document.querySelector('.modal-background');
-const $closeButton = document.querySelector('.close-button');
 const $modalForm = document.querySelector('.modal-form');
 const $modalInput = document.querySelector('.modal-input');
-const $cancelButton = document.querySelector('.cancel-button');
 const $popupMessage = document.querySelector('.popup-message');
 
 $openButton.onclick = () => {
   $modalBackground.classList.add('is-active');
 };
 
-$closeButton.onclick = () => {
-  $modalBackground.classList.remove('is-active');
+$modalBackground.onclick = ({ target }) => {
+  if (target.matches('button')) $modalBackground.classList.remove('is-active');
 };
-
-$cancelButton.onclick = () => {
-  $modalBackground.classList.remove('is-active');
-};
-
-// [$closeButton, $cancelButton].forEach($el => {
-//   $el.onclick = () => $modalBackground.classList.remove('is-active');
-// });
 
 $modalForm.onsubmit = e => {
   e.preventDefault();
-
-  $modalBackground.classList.remove('is-active');
 
   const content = $modalInput.value.trim();
   if (content) $popupMessage.textContent = 'from popup : ' + content;
