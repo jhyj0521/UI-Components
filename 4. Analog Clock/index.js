37f6d62c09bdaf58d6e17f940c628343b006b3e4
jhyj0521 <jhyj3636@gmail.com>
2021-10-15 18:00:42 +0900

    refactor: Analog Clock
    close #5

--- 4. Analog Clock/index.js
+++ 4. Analog Clock/index.js
@@ -1,16 +1,30 @@
+const MINUTES_IN_AN_HOUR = 60;
+const SECONDS_IN_AN_HOUR = 3600;
+const SECONDS_IN_A_MINUTE = 60;
+const DEGREES_PER_HOUR = 30;
+const DEGREES_PER_MINUTE = 6;
+const DEGREES_PER_SECOND = 6;
+
 const $hour = document.querySelector('.hour');
 const $minute = document.querySelector('.minute');
 const $second = document.querySelector('.second');
 
 window.addEventListener('DOMContentLoaded', () => {
   setInterval(() => {
-    const today = new Date();
-    const hour = today.getHours();
-    const minute = today.getMinutes();
-    const second = today.getSeconds();
+    const now = new Date();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const second = now.getSeconds();
 
-    $hour.style.setProperty('--deg', (hour + minute / 60 + second / 3600) * 30);
-    $minute.style.setProperty('--deg', (minute + second / 60) * 6);
-    $second.style.setProperty('--deg', second * 6);
+    $hour.style.setProperty(
+      '--deg',
+      (hour + minute / MINUTES_IN_AN_HOUR + second / SECONDS_IN_AN_HOUR) *
+        DEGREES_PER_HOUR
+    );
+    $minute.style.setProperty(
+      '--deg',
+      (minute + second / SECONDS_IN_A_MINUTE) * DEGREES_PER_MINUTE
+    );
+    $second.style.setProperty('--deg', second * DEGREES_PER_SECOND);
   }, 1000);
 });
