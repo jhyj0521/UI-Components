557799f50a3dace79005051fe337cbdab13c5d8c
hhhyyo <gydnjsrh6923@korea.ac.kr>
2021-10-17 15:37:15 +0900

    refactor: Accordion
    close #10

--- 9. Accordion/index.js
+++ 9. Accordion/index.js
@@ -6,14 +6,14 @@ window.onload = () => {
   $active.lastElementChild.style.height = `${$active.lastElementChild.scrollHeight}px`;
 };
 
-$accordion.onclick = e => {
-  if (!e.target.classList.contains('menu')) return;
+$accordion.onclick = ({ target }) => {
+  if (!target.classList.contains('menu')) return;
 
   [...$accordion.children].forEach($el => {
     $el.classList.remove('active');
     $el.lastElementChild.style.height = '0';
   });
-  e.target.parentNode.classList.add('active');
+  target.parentNode.classList.add('active');
 
-  e.target.nextElementSibling.style.height = `${e.target.nextElementSibling.scrollHeight}px`;
+  target.nextElementSibling.style.height = `${target.nextElementSibling.scrollHeight}px`;
 };
