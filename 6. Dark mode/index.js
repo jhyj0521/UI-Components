bec9fb6676cfc6264b499a5452b3e47e5b844b60
hhhyyo <gydnjsrh6923@korea.ac.kr>
2021-10-17 14:06:55 +0900

    refactor: Dark mode
    close #7

--- 6. Dark mode/index.js
+++ 6. Dark mode/index.js
@@ -1,10 +1,28 @@
-let darked = false;
+let isDarkMode = null;
+const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
 const $body = document.querySelector('body');
 const $toggleButton = document.querySelector('.toggle-button');
 
+window.addEventListener('DOMContentLoaded', () => {
+  $body.style.display = 'none';
+
+  isDarkMode = JSON.parse(localStorage.getItem('isDarkMode'));
+
+  if (isDarkMode === null) {
+    isDarkMode = darkModeMediaQuery.matches;
+    localStorage.setItem('isDarkMode', darkModeMediaQuery.matches);
+  }
+
+  $body.classList.toggle('dark', isDarkMode);
+
+  setTimeout(() => {
+    $body.style.display = 'block';
+  }, 0);
+});
+
 $toggleButton.onclick = () => {
-  darked = !darked;
-  localStorage.setItem('darked', darked);
-  $body.classList.toggle('dark', darked);
+  isDarkMode = !isDarkMode;
+  localStorage.setItem('isDarkMode', isDarkMode);
+  $body.classList.toggle('dark', isDarkMode);
 };
